032c73a7338eb9c47e60c23d956d9af5ddca1cbf
Arfanjaman <141675396+Arfanjaman@users.noreply.github.com>
2024-10-25 23:11:01 +0600

    made some necessary changes to non_linear_methods.cpp

--- non_linear_methods.cpp
+++ non_linear_methods.cpp
@@ -2,45 +2,86 @@
 #include "non_linear_methods.h"
 #include <bits/stdc++.h>
 using namespace std;
-
-void find_ab(double A, double B, double C, double &a, double &b) {
-    b = sqrt(pow(B/A, 2) - 2*C/A);
-    a = -b;
+double maxAmong(const vector<double>& a) {
+    double max = -DBL_MAX;
+    for (int i = 1; i < a.size(); ++i) {
+        if (abs(a[i]) > max) {
+            max = abs(a[i]);
+        }
+    }
+    return max;
 }
 
-double f(double x, double A, double B, double C) {
-    return A * pow(x, 2) + B * x + C;
+void findAB(const vector<double>& a, double& A, double& B) {
+    A = 1 + (1 / abs(a[0])) * maxAmong(a);
+    B = -A;
 }
 
- void biSection() {
-    double A, B, C;
-    cout << "Enter coefficients A, B, C for the equation Ax^2 + Bx + C = 0: ";
-    cin >> A >> B >> C;
-    
-    double dif, x = 0, pre_x, a = 0, b = 0, res;
-    find_ab(A, B, C, a, b);
-    
+double f(const vector<double>& a, double x) {
+    double value = 0;
+    int n = a.size();
+    for (int i = 0; i < n; ++i) {
+        value += a[i] * pow(x, n - i - 1);
+    }
+    return value;
+}
+
+double df(const vector<double>& a, double x) {
+    double value = 0;
+    int n = a.size();
+    for (int i = 0; i < n - 1; ++i) { 
+        value += a[i] * (n - i - 1) * pow(x, n - i - 2);
+    }
+    return value;
+}
+
+void biSection() {
+    int maxPow;
+    cout << "Enter maximum power of the polynomial: ";
+    cin >> maxPow;
+
+    cout << "Enter " << maxPow + 1 << " coefficients: ";
+    vector<double> a(maxPow + 1);
+    for (int i = 0; i <= maxPow; ++i) {
+        cin >> a[i];
+    }
+
+    double A, B;
+    double res;
+    findAB(a, A, B);
+
+    double f_a = f(a, A);
+    double f_b = f(a, B);
+
+    while (f_a * f_b > 0) {
+        B += 1.0;
+        f_b = f(a, B);
+        if (static_cast<int>(A) == static_cast<int>(B)) {
+            cout << "The solution may not be found!" << endl;
+            return;
+        }
+    }
+
     int count = 0;
     bool resultFound = false;
+    double x = 0, f_x;
 
     while (count < 500) {
         ++count;
-        pre_x = x;
-        double f_a = f(a, A, B, C);
-        double f_b = f(b, A, B, C);
-        x = (a + b) / 2;
-        double f_x = f(x, A, B, C);
-        
+        x = (A + B) / 2;
+        f_x = f(a, x);
+
         if (abs(f_x) < 0.0001) {
             res = x;
             resultFound = true;
             break;
         }
-        
+
         if (f_x * f_a > 0) {
-            a = x;
+            A = x;
+            f_a = f_x;
         } else {
-            b = x;
+            B = x;
         }
     }
 
@@ -53,23 +94,42 @@ double f(double x, double A, double B, double C) {
 }
 
 void falsePosition() {
-    double A, B, C;
-    cout << "Enter coefficients A, B, C for the equation Ax^2 + Bx + C = 0: ";
-    cin >> A >> B >> C;
+    int maxPow;
+    cout << "Enter maximum power of the polynomial: ";
+    cin >> maxPow;
 
-    double dif, x = 0, pre_x, a = 0, b = 0, res;
-    find_ab(A, B, C, a, b);
+    cout << "Enter " << maxPow + 1 << " coefficients: ";
+    vector<double> a(maxPow + 1);
+    for (int i = 0; i <= maxPow; ++i) {
+        cin >> a[i];
+    }
+
+    double A, B;
+    findAB(a, A, B);
+
+    double f_a = f(a, A);
+    double f_b = f(a, B);
+
+    while (f_a * f_b > 0) {
+        B += 1.0;
+        f_b = f(a, B);
+        if (static_cast<int>(A) == static_cast<int>(B)) {
+            cout << "The solution may not be found!" << endl;
+            return;
+        }
+    }
 
     int count = 0;
     bool resultFound = false;
+    double x = 0, f_x, res;
 
     while (count < 500) {
         ++count;
-        pre_x = x;
-        double f_a = f(a, A, B, C);
-        double f_b = f(b, A, B, C);
-        x = (a * f_b - b * f_a) / (f_b - f_a);
-        double f_x = f(x, A, B, C);
+        f_a = f(a, A);
+        f_b = f(a, B);
+        
+        x = (A * f_b - B * f_a) / (f_b - f_a);
+        f_x = f(a, x);
 
         if (abs(f_x) < 0.0001) {
             res = x;
@@ -78,30 +138,34 @@ void falsePosition() {
         }
 
         if (f_x * f_a > 0) {
-            a = x;
+            A = x;  
+            f_a = f_x;
         } else {
-            b = x;
+            B = x;  
         }
     }
 
-    if (resultFound) {
-        cout << "Result: " << res << endl;
+    if (resultFound){
+        cout << "Result: " << (res == 0 ? 0 : res) << endl;
         cout << "Iterations: " << count << endl;
-    } else {
+    }
+    else {
         cout << "Result not found in 500 iterations." << endl;
     }
 }
 
-double df(double x, double A, double B, double C) {
-    return 2 * A * x + B;
-}
-
 void newton() {
-    double A, B, C;
-    cout << "Enter coefficients A, B, C for the equation Ax^2 + Bx + C = 0: ";
-    cin >> A >> B >> C;
+    int maxPow;
+    cout << "Enter maximum power of the polynomial: ";
+    cin >> maxPow;
 
-    double x = 0, pre_x = 4, f_pre_x, df_pre_x;
+    cout << "Enter " << maxPow + 1 << " coefficients: ";
+    vector<double> a(maxPow + 1);
+    for (int i = 0; i <= maxPow; ++i) {
+        cin >> a[i];
+    }
+
+    double x = 0, pre_x = 4;
     int count = 0;
     double res;
     int iteration;
@@ -109,7 +173,7 @@ void newton() {
 
     while (count < 500) {
         ++count;
-        f_pre_x = f(pre_x, A, B, C);
+        double f_pre_x = f(a, pre_x);
         if (abs(f_pre_x) < 0.0001 && !gotResult) {
             res = pre_x;
             iteration = count;
@@ -117,7 +181,7 @@ void newton() {
             break;
         }
 
-        df_pre_x = df(pre_x, A, B, C);
+        double df_pre_x = df(a, pre_x);
         if (df_pre_x == 0) {
             cout << "Can't be calculated. Derivative is zero!" << endl;
             return;
@@ -134,13 +198,20 @@ void newton() {
     }
 }
 
-
 void secant() {
-    double A, B, C;
-    cout << "Enter coefficients A, B, C for the equation Ax^2 + Bx + C = 0: ";
-    cin >> A >> B >> C;
+    int maxPow;
+    cout << "Enter maximum power of the polynomial: ";
+    cin >> maxPow;
 
-    double x = 0, a = 5, b = 10, f_x, f_a, f_b;
+    cout << "Enter " << maxPow + 1 << " coefficients: ";
+    vector<double> a(maxPow + 1);
+    for (int i = 0; i <= maxPow; ++i) {
+        cin >> a[i];
+    }
+
+    double a_val = 1; 
+    double b_val = 10; 
+    double x = 0, f_x, f_a, f_b;
     int count = 0;
     double res;
     int iteration;
@@ -149,15 +220,15 @@ void secant() {
     while (count < 500) {
         ++count; 
 
-        f_a = f(a, A, B, C);
-        f_b = f(b, A, B, C);
+        f_a = f(a, a_val);
+        f_b = f(a, b_val);
         if (abs(f_a - f_b) < 1e-10) {
             cout << "Function values are too close. No solution found." << endl;
             return;
         }
 
-        x = b - (f_b * (b - a)) / (f_b - f_a);
-        f_x = f(x, A, B, C);
+        x = b_val - (f_b * (b_val - a_val)) / (f_b - f_a);
+        f_x = f(a, x);
 
         if (abs(f_x) < 0.0001 && !gotResult) {
             res = x;
@@ -166,8 +237,8 @@ void secant() {
             break;
         }
         
-        a = b;
-        b = x;
+        a_val = b_val;
+        b_val = x;
     }
 
     if (gotResult) {
